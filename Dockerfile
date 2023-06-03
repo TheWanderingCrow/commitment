345dfdfc63cbc596ebaf94765910e6df6eb96070
TheWanderingCrow <torluck.kculrot@gmail.com>
2023-06-03 09:28:54 -0400

    Well this took a hard left from the original project lol

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.10-alpine3.15
-WORKDIR /app
-COPY requirements.txt commit.py commit_messages.txt index.html /app/
-COPY static /app/static/
-RUN pip install --no-cache-dir -r requirements.txt
-CMD [ "python", "/app/commit.py" ]
---
+++ commit.php
@@ -0,0 +1,31 @@
+<?php
+
+$default_port = 5000;
+$names = ['Ben', 'Shea', 'Mike', 'Patrick'];
+
+$messages_file = file(__DIR__."/commit_messages.txt");
+$messages = [];
+
+// generate a hashtable for all the messages
+foreach ($messages_file as $line) {
+    $messages[md5($line)] = $line;
+}
+
+
+function fill_line($message) {
+    global $names;
+    $message = preg_replace('#XNAMEX#', $names[array_rand($names)], $message);
+    $message = preg_replace('#XUPPERNAMEX#', strtoupper($names[array_rand($names)]), $message);
+    $message = preg_replace('#XLOWERNAMEX#', strtolower($names[array_rand($names)]), $message);
+    $message = preg_replace('#XNUMX#', rand(), $message);
+
+    return $message;
+}
+
+function return_message() {
+    global $messages;
+    $hash = array_rand($messages);
+    $message = fill_line($messages[$hash]);
+
+    return [$hash, $message];
+}
